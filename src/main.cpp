697fd671619bf17a609a97d9e5e260213449b747
yarten <yfic@qq.com>
2018-04-24 11:44:05 +0800

    修改cmake，增加输出一个静态的库

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,23 @@ int main()
 {
     cout << OpenCL::GetDevicesInformation() << endl;
 
-//    OpenCL openCL;
-//    OpenCL::Program::Kernel kernel = openCL("./Vadd.cl")("vadd");
-//
-//    size_t rows = 200, cols = 300;
-//    kernel.SetSize(rows, cols);
-//    Buffer<float> a(rows * cols), b(rows * cols), c(rows * cols);
-//
-//    for(size_t i = 0; i < rows; i++)
-//        for(size_t j = 0; j < cols; j++)
-//        {
-//            a[i * cols + j] = 1;
-//            b[i * cols + j] = 2;
-//        }
-//
-//    Event event = kernel(c, a, b);
-//
-//    cout << endl << event.ExecutionTime << endl;
+    OpenCL openCL;
+    OpenCL::Program::Kernel kernel = openCL("./Vadd.cl")("vadd");
 
-    Buffer<size_t> v(3);
-    v = 4;
-    cout << v << endl;
+    size_t rows = 200, cols = 300;
+    kernel.SetSize(rows, cols);
+    Buffer<float> a(rows * cols), b(rows * cols), c(rows * cols);
+
+    for(size_t i = 0; i < rows; i++)
+        for(size_t j = 0; j < cols; j++)
+        {
+            a[i * cols + j] = 1;
+            b[i * cols + j] = 2;
+        }
+
+    Event event = kernel(c, a, b);
+
+    cout << endl << event.ExecutionTime << endl;
 
     return 0;
 }
